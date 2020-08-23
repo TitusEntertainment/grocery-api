21223c32188abd8106497e10a702f1a54eb74a7d
Eddie <properllerbucher@gmail.com>
2020-08-23 22:33:17 +0200

    new handler for incoming requests

---
+++ src/helpers/lists/listInputHandler.js
@@ -0,0 +1,43 @@
+const { ERROR_BAD_REQUEST } = require('../error.json');
+const { validate: uuidValidate } = require('uuid');
+
+const yup = require('yup');
+
+const verifyListItems = (list) => {
+  const listSchema = yup.array().of(
+    yup.object().shape({
+      name: yup.string().required(),
+      amount: yup.number().required().positive().integer().min(1).max(parseInt(process.env.LIST_SIZE)),
+    }),
+  );
+
+  try {
+    listSchema.validate(list);
+  } catch (error) {
+    return { ok: false, ERROR_BAD_REQUEST, reason: error };
+  }
+
+  return { ok: true, error: null };
+};
+
+const verifyInvites = (invites) => {
+  const inviteSchema = yup.array().of(
+    yup.object().shape({
+      userId: yup
+        .string()
+        .required()
+        .test((value) => uuidValidate(value)),
+      isSent: yup.boolean().default(false),
+    }),
+  );
+
+  try {
+    inviteSchema.validate(invites);
+  } catch (error) {
+    return { ok: false, ERROR_BAD_REQUEST, reason: error };
+  }
+
+  return { ok: true, error: null };
+};
+
+module.exports = { verifyListItems, verifyInvites };
