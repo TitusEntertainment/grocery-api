4ccbec4c5c7419aceca07d84582d57c42655ea7f
Eddie Englund <properllerbucher@gmail.com>
2021-01-17 21:59:16 +0100

    owo

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-const express = require('express');
-const helmet = require('helmet');
-require('dotenv').config();
-const { setupDB } = require('./db/db');
+const express = require("express");
+const helmet = require("helmet");
+require("dotenv").config();
+const { setupDB } = require("./db/db");
 const PORT = process.env.PORT || 1982;
-const { setupRoutes } = require('./helpers/routes');
+const { setupRoutes } = require("./helpers/routes");
 
 const init = async () => {
   await setupDB(process.env.MONGO_URI, process.env.DB_NAME);
   const app = express();
 
-  app.use(express.json({ limit: '100kb', strict: true, type: 'application/json' }));
+  app.use(
+    express.json({ limit: "100kb", strict: true, type: "application/json" })
+  );
   app.use(helmet());
 
   setupRoutes(app);
@@ -17,4 +19,7 @@ const init = async () => {
   app.listen(PORT, () => console.log(`API has started on port: ${PORT}`));
 };
 
-init().catch((err) => console.log(`Failed to start API: ${err}`));
+init().catch((err) => {
+  console.error(err);
+  console.log(`Failed to start API: ${err}`);
+});
